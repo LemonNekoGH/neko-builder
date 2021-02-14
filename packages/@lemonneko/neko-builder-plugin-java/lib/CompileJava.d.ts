dd775bb4876417054038d37fd31f735df9f92a94
LemonNeko <chheese048@gmail.com>
2021-02-14 22:15:36 +0800

    :construction:移除构建好之后的代码

--- packages/@lemonneko/neko-builder-plugin-java/lib/CompileJava.d.ts
+++
@@ -1,4 +0,0 @@
-import { Task } from '@lemonneko/neko-builder/lib/task';
-export declare class CompileJava extends Task {
-    action: () => void;
-}
--- packages/@lemonneko/neko-builder-plugin-java/lib/PluginJava.d.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin } from '@lemonneko/neko-builder/lib/plugin';
-import { DefaultProject } from '@lemonneko/neko-builder/lib/config';
-export declare class PluginJava implements Plugin {
-    apply(t: DefaultProject): void;
-}
